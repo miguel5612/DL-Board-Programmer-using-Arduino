5d0123e7ac4608d2562ce99c80a26b49348448e6
miguel <miguelangelcu@ufps.edu.co>
2018-12-04 17:48:01 -0500

    Se realiza la busqueda de puertos y la conexion :)

---
+++ DLProgram/DLProgram/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DLProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.ddPorts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ddBaudRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(484, 31);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Conectar!";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ddPorts
+            // 
+            this.ddPorts.FormattingEnabled = true;
+            this.ddPorts.Location = new System.Drawing.Point(100, 34);
+            this.ddPorts.Name = "ddPorts";
+            this.ddPorts.Size = new System.Drawing.Size(121, 21);
+            this.ddPorts.TabIndex = 1;
+            this.ddPorts.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Puerto Serie";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ddBaudRate
+            // 
+            this.ddBaudRate.FormattingEnabled = true;
+            this.ddBaudRate.Location = new System.Drawing.Point(357, 33);
+            this.ddBaudRate.Name = "ddBaudRate";
+            this.ddBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.ddBaudRate.TabIndex = 3;
+            this.ddBaudRate.SelectedIndexChanged += new System.EventHandler(this.ddBaudRate_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "BaudRate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ddBaudRate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ddPorts);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox ddPorts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ddBaudRate;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DLProgram/DLProgram/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace DLProgram
+{
+    public partial class Form1 : Form
+    {
+        SerialPort ArduinoPort = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+            //fillPortListe: Con esta funcion se llena la lista de puertos que se puede seleccionar en el DropDown
+            fillPortListe();
+            //fillBaudRate: Con esta funcion se llena los baudrate
+            fillBaudRate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            try
+            {
+                if(btnConnect.Text == "Conectar!")
+                {
+                    //Conectar 
+
+                    ArduinoPort.PortName = ddPorts.Text;
+                    ArduinoPort.BaudRate = Int32.Parse(ddBaudRate.Text);                   
+                    ArduinoPort.DataBits = 8;
+                    ArduinoPort.Parity = Parity.None;
+                    ArduinoPort.StopBits = StopBits.One;
+                    ArduinoPort.Handshake = Handshake.None;
+                    try
+                    {
+                        ArduinoPort.Open();
+                        btnConnect.Text = "Desconectar!";
+                        btnConnect.Enabled = true;
+                    }
+                    catch(Exception)
+                    {
+                        throw;
+                    }
+                }
+                else
+                {
+                    //Desconectar
+                    ArduinoPort.Close();
+                    btnConnect.Text = "Conectar!";
+                    btnConnect.Enabled = true;
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void fillBaudRate()
+        {
+            ddBaudRate.Items.Add("9600");
+            ddBaudRate.Items.Add("57600");
+            ddBaudRate.Items.Add("115200");
+            ddBaudRate.Items.Add("250000");
+            ddBaudRate.SelectedIndex = 0;
+        }
+        private void fillPortListe()
+        {
+            ddPorts.Items.Add("COM3");
+            foreach (String port in SerialPort.GetPortNames())
+            {
+                ddPorts.Items.Add(port);
+            }
+            ddPorts.SelectedIndex = 0;
+        }
+
+        private void ddBaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
